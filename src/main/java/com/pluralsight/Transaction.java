46113b41dd3ed35f4abeb219a08fbfc89f1fa3c5
Aliza's <aliza.visuals@gmail.com>
2025-10-13 13:32:26 -0500

    Transaction constructor and getters added.

---
+++ src/main/java/com/pluralsight/Transaction.java
@@ -0,0 +1,45 @@
+package com.pluralsight;
+
+public class Transaction {
+    // declaring the fields
+    private String date;
+    private String time;
+    private String description;
+    private String vendor;
+    private double amount;
+
+    // passing the parameters for the constructor
+    public Transaction (String date, String time, String description, String Vendor, double amount) {
+
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String toString() {
+        return date + " | " + time + " | " + description + " | " + vendor + " | " + amount;
+    }
+
+}
