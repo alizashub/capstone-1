ba0da0463190f7b0b1800054714d8963b39d9a6f
Aliza's <aliza.visuals@gmail.com>
2025-10-16 21:43:53 -0500

    Report Methods Updated.

--- src/main/java/com/pluralsight/Ledger.java
+++ src/main/java/com/pluralsight/Ledger.java
@@ -1,5 +1,6 @@
 package com.pluralsight;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
@@ -31,9 +32,9 @@ public class Ledger {
             System.out.println("                  HOME MENU                           ");
             System.out.println("                 -----------                          ");
             System.out.println("What Shall We Do Today? ");
-            System.out.println("A) Add A Deposit ");
-            System.out.println("B) Make A Payment ");
-            System.out.println("C) Go To The Ledger ");
+            System.out.println("D) Add A Deposit ");
+            System.out.println("P) Make A Payment ");
+            System.out.println("L) Go To The Ledger ");
             System.out.println("X) Exit The Program ");
             System.out.print("\n" + userName + "," + " Type The LETTER Of Your Choice From Above To Get Started : ");
 
@@ -52,13 +53,13 @@ public class Ledger {
 
 
             switch (choice) {
-                case "A":
+                case "D":
                     addDeposit();
                     break;
-                case "B":
+                case "P":
                     makePayment();
                     break;
-                case "C":
+                case "L":
                     showLedger();
                     break;
                 case "X":
@@ -73,6 +74,7 @@ public class Ledger {
 
     private void askUserName() {
         System.out.print("Hey There! What's Your Name?");
+        // take user input and save it to the userName variable
         userName = myScanner.next().trim();
 
         while (userName.isEmpty()) {
@@ -301,123 +303,154 @@ public class Ledger {
         }
     }
 
+
+    // shows transaction from start of the current month up to today
     private void showMonthToDate() {
         System.out.println("\n");
         System.out.println("   MONTH TO DATE REPORT  ");
 
         LocalDate today = LocalDate.now();
-        LocalDate firstDayofMonth = today.withDayOfMonth(1);
+        LocalDate firstDayofMonth = today.withDayOfMonth(1); // create a new object with the same year and month, but day set to 1
+        System.out.println("Transactions From " + firstDayofMonth + " " + " to " +today);
+        boolean matchFound = false; // the varible matchFound starts with no matches
 
-        boolean matchFound = false;
-
+        // looping backwards
         for (int i = transactions.size() - 1; i >= 0; i--) {
+            // gets one transaction object from the array list at index i
             Transaction t = transactions.get(i);
+            // turns the string into a localdate object called trasactiondate
             LocalDate transactionDate = LocalDate.parse(t.getDate());
-            if ((transactionDate.isEqual(firstDayofMonth) || transactionDate.isAfter(firstDayofMonth)) && (transactionDate.isEqual(today) || transactionDate.isBefore(today)))
+            //checks if the date is on or after the first day of the month && if the date is on or before today ( to find if the transaction happened this month )
+            if (!transactionDate.isBefore(firstDayofMonth) && !transactionDate.isAfter(today)) {
                 System.out.println(t.getDate() + "|" + t.getTime() + "|" + t.getDescription() + "|" + t.getVendor() + "|" + t.getAmount());
-            matchFound = true;
-        }
+                matchFound = true;
+            }
+        } // loop finished
+        // check if after the loop finsihes no matches are found
         if (!matchFound) {
-            System.out.println("No transactions found for this month to date.");
+            System.out.println("No transactions Found For This month To Date.");
         }
     }
 
+    // show transactions from the previous month
     private void showPreviousMonth() {
-        System.out.println("\n=== PREVIOUS MONTH TRANSACTIONS ===");
+
+        System.out.println("\n");
+        System.out.println("   PREVIOUS MONTH TRANSACTIONS  ");
+
+
+        // current date eg. 10/16/2025
         LocalDate today = LocalDate.now();
+        // 1st day of the current month eg. 1/6/2015
         LocalDate firstDayOfCurrentMonth = today.withDayOfMonth(1);
+        // subtract 1 day from the 1st of the current month to find the last day of the previous month. eg. 10/30/2025
         LocalDate lastDayOfPreviousMonth = firstDayOfCurrentMonth.minusDays(1);
+        // sets day to 1 for the previous month eg. 10/1/2025
         LocalDate firstDayOfPreviousMonth = lastDayOfPreviousMonth.withDayOfMonth(1);
 
-        boolean matchFound = false;
+        System.out.println("\nTransactions For " + lastDayOfPreviousMonth.getMonth() + " :");
 
+        boolean matchFound = false;
+        // loops backwards through the transactions
         for (int i = transactions.size() - 1; i >= 0; i--) {
             Transaction t = transactions.get(i);
+            // converts string to localdate object
             LocalDate transactionDate = LocalDate.parse(t.getDate());
 
-            boolean onOrAfterStartMonth = transactionDate.isEqual(firstDayOfPreviousMonth) || transactionDate.isAfter(firstDayOfPreviousMonth);
-            boolean onOrBeforeEndMonth = transactionDate.isEqual(lastDayOfPreviousMonth) || transactionDate.isBefore(lastDayOfPreviousMonth);
-
-            if (onOrAfterStartMonth && onOrBeforeEndMonth) {
+            if (!transactionDate.isBefore(firstDayOfPreviousMonth) && !transactionDate.isAfter(lastDayOfPreviousMonth)) {
                 System.out.println(t.getDate() + "|" + t.getTime() + "|" + t.getDescription() + "|" + t.getVendor() + "|" + t.getAmount());
+                matchFound = true;
+
             }
+        }
+        if (!matchFound) {
+            System.out.println("No Transactions Found For " + lastDayOfPreviousMonth.getMonth());
         }
 
     }
 
+    // show transaction from Jan 1st of the current year to the current date of the current year
     private void showYearToDate() {
+        System.out.println("\n");
+        System.out.println("   YEAR TO DATE TRANSACTIONS   ");
+
+        // todays date eg. 10/16/2025
         LocalDate today = LocalDate.now();
+        //sets firstdayoftheyear to jan 1st of the current year eg. 1/1/2025
         LocalDate firstDayOfYear = today.withDayOfYear(1);
+        // creating a header
+        System.out.println("\nTransactions From " + firstDayOfYear.getMonth() + " " + firstDayOfYear.getYear() + " to " + today.getMonth() + " " + today.getYear());
 
         boolean matchFound = false;
-
+        // looping backwards through the transactions
         for (int i = transactions.size() - 1; i >= 0; i--) {
             Transaction t = transactions.get(i);
-
+            // converts date string to localdate object
             LocalDate transactionDate = LocalDate.parse(t.getDate());
-
-            boolean onOrAfterStartYear = transactionDate.isEqual(firstDayOfYear) || transactionDate.isAfter(firstDayOfYear);
-            boolean onOrBeforeTodayYear = transactionDate.isEqual(today) || transactionDate.isBefore(today);
-
-            if (onOrAfterStartYear & onOrBeforeTodayYear) {
+            // checks to see if the date is jan 1st or after and if the date if today or earlier ( eg. range created with Minimum : Jan 1st & Maximum : 16th October )
+            if ((!transactionDate.isBefore(firstDayOfYear)) && !transactionDate.isAfter(today)) {
                 System.out.println(t.getDate() + "|" + t.getTime() + "|" + t.getDescription() + "|" + t.getVendor() + "|" + t.getAmount());
-
                 matchFound = true;
             }
         }
-
         if (!matchFound) {
-            System.out.println("No transactions found for Year to Date.");
+            System.out.println("No transactions Found For" + firstDayOfYear.getMonth() + " " + firstDayOfYear.getYear() + " to " + today.getMonth() + " " + today.getYear());
         }
-    }
-
-    private void showPreviousYear() {
-        System.out.println("\n=== PREVIOUS YEAR TRANSACTIONS ===");
-
-        LocalDate today = LocalDate.now();
-
-        LocalDate firstDayOfThePreviousYear = today.minusYears(1).withDayOfYear(1);
-        LocalDate lastDayPreviousYear = today.withDayOfYear(1).minusDays(1);
-
-        boolean matchFound = false;
-
-        for (int i = transactions.size() - 1; i >= 0; i--) {
-            Transaction t = transactions.get(i);
-
-            LocalDate transactionDate = LocalDate.parse(t.getDate());
-
-            boolean onOrAfterStartPreviousYear = transactionDate.isEqual(firstDayOfThePreviousYear) || transactionDate.isAfter(firstDayOfThePreviousYear);
-            boolean onOrBeforeEndPreviousYear = transactionDate.isEqual(lastDayPreviousYear) || transactionDate.isBefore(lastDayPreviousYear);
-
-            if (onOrAfterStartPreviousYear & onOrBeforeEndPreviousYear) {
-                System.out.println(t.getDate() + "|" + t.getTime() + "|" + t.getDescription() + "|" + t.getVendor() + "|" + t.getAmount());
-
-                matchFound = true;
-            }
         }
 
-        if (!matchFound) {
-            System.out.println("No transactions found for previous year.");
-        }
-    }
+    // show transactions from the previous year
+    private void showPreviousYear () {
+        System.out.println("\n");
+        System.out.println("   PREVIOUS YEAR TRANSACTIONS   ");
 
-    private void searchByVendor() {
-        System.out.println("Enter vendor name to search: ");
-        String searchVendor = myScanner.nextLine().trim();
+            //  todays date eg. 10/16/2025
+            LocalDate today = LocalDate.now();
+            // changes the year  eg. 10/16/2024 and then changes the day of that year to the 1st day eg. 1/1/2024
+            LocalDate firstDayOfThePreviousYear = today.minusYears(1).withDayOfYear(1);
+            // changes current date to day 1, month 1 eg. 1/1/2025 and then subtracts one day eg. 12/31/2024
+            LocalDate lastDayPreviousYear = today.withDayOfYear(1).minusDays(1);
+            System.out.println("Transactions From " + firstDayOfThePreviousYear.getYear());
 
-        boolean matchFound = false;
+            boolean matchFound = false;
+            // looping backwards
+            for (int i = transactions.size() - 1; i >= 0; i--) {
+                Transaction t = transactions.get(i);
+                // convert string to localdate object
+                LocalDate transactionDate = LocalDate.parse(t.getDate());
+                // checks if the date is within the previous year, not before and not after the last day of the previous year. eg. between 1st Jan 2024 to 31st Dec 2024.
+                if (!transactionDate.isBefore(firstDayOfThePreviousYear) && !transactionDate.isAfter(lastDayPreviousYear)) {
+                    System.out.println(t.getDate() + "|" + t.getTime() + "|" + t.getDescription() + "|" + t.getVendor() + "|" + t.getAmount());
+                    matchFound = true; }
 
-        for (int i = transactions.size() - 1; i >= 0; i--) {
-            Transaction t = transactions.get(i);
-            if (t.getVendor().equalsIgnoreCase(searchVendor)) {
-                System.out.println(t.getDate() + "|" + t.getTime() + "|" + t.getDescription() + "|" + t.getVendor() + "|" + t.getAmount());
-
-                matchFound = true;
-
-
+                }
+            if (!matchFound){
+                System.out.println("No transactions Found For " + firstDayOfThePreviousYear.getYear());
             }
 
+    }
+
+    // show transaction from specifc vendors
+        private void searchByVendor () {
+            System.out.println("\n");
+            System.out.println("   VENDOR TRANSACTIONS   ");
+            System.out.println("Enter Vendor Name To Search: ");
+            String searchVendor = myScanner.nextLine().trim();
+            System.out.println("Transactions From "+ searchVendor );
+
+            boolean matchFound = false;
+            // looping backwards
+            for (int i = transactions.size() - 1; i >= 0; i--) {
+                Transaction t = transactions.get(i);
+
+                if (t.getVendor().equalsIgnoreCase(searchVendor)) {
+                    System.out.println(t.getDate() + "|" + t.getTime() + "|" + t.getDescription() + "|" + t.getVendor() + "|" + t.getAmount());
+                    matchFound = true; }
+                }
+
+            if (!matchFound) {
+                System.out.println("No transactions Found For " + searchVendor);
+
+            }
         }
     }
-}
 
