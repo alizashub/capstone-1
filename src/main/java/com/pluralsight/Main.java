cb5ef2f1f2ce0b6099e9e945b82edfc251843689
Aliza's <aliza.visuals@gmail.com>
2025-10-17 10:31:49 -0500

    Back Looping Fixed.

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,7 +1,7 @@
 package com.pluralsight;
 public class Main {
     public static void main(String[] args) {
-        Ledger ledgerClass = new Ledger();
-        ledgerClass.home();
+        Ledger ledger = new Ledger();
+        ledger.home();
     }
 }
